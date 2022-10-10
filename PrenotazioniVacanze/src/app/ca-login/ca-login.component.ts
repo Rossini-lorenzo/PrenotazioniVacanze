57a670fb2e382229d9fe460ac9e71f36a5be785b
lorenzo <105223252+Rossini-lorenzo@users.noreply.github.com>
2022-10-10 17:07:25 +0200

    1.0

---
+++ PrenotazioniVacanze/src/app/ca-login/ca-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ca-login',
+  templateUrl: './ca-login.component.html',
+  styleUrls: ['./ca-login.component.css']
+})
+export class CaLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
