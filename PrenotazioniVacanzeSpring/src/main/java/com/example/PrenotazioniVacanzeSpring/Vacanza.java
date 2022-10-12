aa34dc6edafe5f7f26150f754386efb0c6397566
Ferdinando Errichiello <ferdinautoferfigno@gmail.com>
2022-10-12 12:02:51 +0200

    Spring Vacanza

--- PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Vacanza.java
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Vacanza.java
@@ -1,14 +1,38 @@
 package com.example.PrenotazioniVacanzeSpring;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Vacanza {
+    
+    public enum tipoMezzo {pullman,treno,aereo};
+    
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private int codAlloggio;
     private int codViaggio;
     private int codPacchetto;
+    private String destinazione;
+    private String descrizione;
+    private int nMaxPartecipanti;
+    private int nStanzeLetto;
+    private int nBagni;
+    private int nLetti;
+    private Date orarioPartenza;
+    private Date orarioArrivo;
+    private int nBiglietti;
+    private String luogoPartenza;
+    @Enumerated(EnumType.STRING)
+    private tipoMezzo tipoMezzo;
+    private Date dataInizio;
+    private Date dataFine;
+    private int scontoOfferto;
 }
