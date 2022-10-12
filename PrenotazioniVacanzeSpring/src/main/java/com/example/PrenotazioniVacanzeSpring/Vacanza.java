4d5a18aeeb31e66f48f81dd2059a07281da766b2
Ferdinando Errichiello <ferdinautoferfigno@gmail.com>
2022-10-12 12:28:28 +0200

    fix

--- PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Vacanza.java
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Vacanza.java
@@ -8,8 +8,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Vacanza")
 public class Vacanza {
     
     public enum tipoMezzo {pullman,treno,aereo};
