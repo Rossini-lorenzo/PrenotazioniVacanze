d723fae66c1b5bcfbfc615848748f03fbd3c6a02
lorenzo <105223252+Rossini-lorenzo@users.noreply.github.com>
2022-10-12 09:30:23 +0200

    1.0

---
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/PrenotazioniVacanzeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrenotazioniVacanzeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioniVacanzeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotazioniVacanzeSpringApplication.class, args);
+	}
+
+}
