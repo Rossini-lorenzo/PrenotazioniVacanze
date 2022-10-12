eb5edd02bbaad0413217fa6e0e36672c7adbf77e
Ferdinando Errichiello <ferdinautoferfigno@gmail.com>
2022-10-12 12:05:56 +0200

    Spring storicoPrenotazione

--- PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/StoricoPrenotazione.java
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/StoricoPrenotazione.java
@@ -1,5 +1,24 @@
 package com.example.PrenotazioniVacanzeSpring;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class StoricoPrenotazione {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private int committente;
+    private int prenotazione;
+    private int nPartecipanti;
+    private float prezzo;
+    private String destinazione;
+    private String descrizione;
+    private Date dataInizio;
+    private Date dataFine;
 }
