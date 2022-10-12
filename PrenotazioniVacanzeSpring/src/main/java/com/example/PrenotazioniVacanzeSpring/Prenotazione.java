412e0586c166bba52f4ef8a35daa883d58c02928
Ferdinando Errichiello <ferdinautoferfigno@gmail.com>
2022-10-12 11:02:17 +0200

    Spring prenotazione

--- PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Prenotazione.java
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Prenotazione.java
@@ -1,5 +1,46 @@
 package com.example.PrenotazioniVacanzeSpring;
 
-public class Prenotazione {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Prenotazione {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private int committente;
+    private int offerta;
+    private int nPartecipanti;
+    private String titolo;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public int getCommittente() {
+        return committente;
+    }
+    public void setCommittente(int committente) {
+        this.committente = committente;
+    }
+    public int getOfferta() {
+        return offerta;
+    }
+    public void setOfferta(int offerta) {
+        this.offerta = offerta;
+    }
+    public int getnPartecipanti() {
+        return nPartecipanti;
+    }
+    public void setnPartecipanti(int nPartecipanti) {
+        this.nPartecipanti = nPartecipanti;
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+    
 }
--- PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Vacanza.java
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Vacanza.java
@@ -1,5 +1,14 @@
 package com.example.PrenotazioniVacanzeSpring;
 
-public class Vacanza {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Vacanza {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private int codAlloggio;
+    private int codViaggio;
+    private int codPacchetto;
 }
