798e34348f8f2346ac9afd319663aaaa50d79ffb
lorenzo <105223252+Rossini-lorenzo@users.noreply.github.com>
2022-10-12 11:01:21 +0200

    1.0

--- PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Offerta.java
+++ PrenotazioniVacanzeSpring/src/main/java/com/example/PrenotazioniVacanzeSpring/Offerta.java
@@ -1,5 +1,36 @@
 package com.example.PrenotazioniVacanzeSpring;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+/*
+ * CREATE TABLE `Offerta` (
+`codOfferta` INT NOT NULL AUTO_INCREMENT,
+`prezzo` INT NOT NULL,
+`vacanza` INT,
+`dataInizio` DATE NOT NULL,
+`dataFine` DATE NOT NULL,
+PRIMARY KEY (`codOfferta`),
+FOREIGN KEY (`vacanza`) REFERENCES Vacanza(`idVacanza`));
+ */
+
+
+@Entity // This tells Hibernate to make a table out of this class
 public class Offerta {
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	 private Integer codOfferta;
+	 private Integer prezzo;
+	 private Date DateInizio;
+	 private Date DataFine;
+	 
 
 }
