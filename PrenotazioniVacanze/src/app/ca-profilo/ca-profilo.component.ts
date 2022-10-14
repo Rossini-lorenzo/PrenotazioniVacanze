7f2e60956fbd9a0bad8376d7b8a7ed340dbc644c
Ferdinando Errichiello <ferdinautoferfigno@gmail.com>
2022-10-14 17:20:34 +0200

    Aggiunta pagina prenotazioni
    da collegare al database

--- PrenotazioniVacanze/src/app/ca-profilo/ca-profilo.component.ts
+++ PrenotazioniVacanze/src/app/ca-profilo/ca-profilo.component.ts
@@ -7,15 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaProfiloComponent implements OnInit {
 
-  modificaNome=false;
-  modificaCognome=false;
   modificaEmail=false;
-  modificaDataNascita=false;
-  modificaIndirizzo=false;
   modificaTelefono=false;
-  modificaCodiceCartaIdentita=false;
-  modificaSesso=false;
-  modificaNomeUtente=false;
   modificaPassword=false;
 
   constructor() { }
@@ -23,25 +16,11 @@ export class CaProfiloComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  toggleNome() {
-    if(this.modificaNome==true) {
-      this.modificaNome=false;
-    } else {
-      this.modificaNome=true;
-    }
-  }
   toggleEmail() {
     if(this.modificaEmail==true) {
       this.modificaEmail=false;
     } else {
       this.modificaEmail=true;
-    }
-  }
-  toggleDataNascita() {
-    if(this.modificaDataNascita==true) {
-      this.modificaDataNascita=false;
-    } else {
-      this.modificaDataNascita=true;
     }
   }
   toggleTelefono() {
@@ -51,20 +30,6 @@ export class CaProfiloComponent implements OnInit {
       this.modificaTelefono=true;
     }
   }
-  toggleCodiceCartaIdentita() {
-    if(this.modificaCodiceCartaIdentita==true) {
-      this.modificaCodiceCartaIdentita=false;
-    } else {
-      this.modificaCodiceCartaIdentita=true;
-    }
-  }
-  toggleNomeUtente() {
-    if(this.modificaNomeUtente==true) {
-      this.modificaNomeUtente=false;
-    } else {
-      this.modificaNomeUtente=true;
-    }
-  }
   togglePassword() {
     if(this.modificaPassword==true) {
       this.modificaPassword=false;
@@ -72,37 +37,9 @@ export class CaProfiloComponent implements OnInit {
       this.modificaPassword=true;
     }
   }
-  toggleSesso() {
-    if(this.modificaSesso==true) {
-      this.modificaSesso=false;
-    } else {
-      this.modificaSesso=true;
-    }
-  }
-  toggleCognome() {
-    if(this.modificaCognome==true) {
-      this.modificaCognome=false;
-    } else {
-      this.modificaCognome=true;
-    }
-  }
-  toggleIndirizzo() {
-    if(this.modificaIndirizzo==true) {
-      this.modificaIndirizzo=false;
-    } else {
-      this.modificaIndirizzo=true;
-    }
-  }
   bAnnulla() {
-    this.modificaNome=false;
-    this.modificaCognome=false;
     this.modificaEmail=false;
-    this.modificaDataNascita=false;
-    this.modificaIndirizzo=false;
     this.modificaTelefono=false;
-    this.modificaCodiceCartaIdentita=false;
-    this.modificaSesso=false;
-    this.modificaNomeUtente=false;
     this.modificaPassword=false;
   }
 }
